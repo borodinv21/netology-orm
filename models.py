a78abef9358d15f08cd767c904c6afdc4f96530a
borodinv <vikc003@mail.ru>
2024-06-12 16:51:37 +0500

    initial commit

---
+++ models.py
@@ -0,0 +1,66 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Publisher(Base):
+    __tablename__ = 'publisher'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=250), unique=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=250), unique=True, nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+
+    publisher = relationship(Publisher, backref='book')
+
+    def __str__(self):
+        return f'{self.id} - ({self.title}, {self.publisher_id})'
+
+class Shop(Base):
+    __tablename__ = 'shop'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=250), unique=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+class Stock(Base):
+    __tablename__ = 'stock'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+    book = relationship(Book, backref='stock')
+    shop = relationship(Shop, backref='stock')
+
+    def __str__(self):
+        return f'{self.id} - (BookID: {self.id_book}, ShopID: {self.id_shop}, Count - {self.count})'
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
+
+    stock = relationship(Stock, backref='sale')
+
+    def __str__(self):
+        return f'{self.id} - (StockID: {self.id_stock}, Price - {self.price}, Count - {self.count}, DateSale - {self.date_sale})'
+
+
